b1a888d2913efbdb947316dcdf7ab3ecf15f261b
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-08-05 17:44:20 +0200

    Move GovernanceRelays and tests

---
+++ packages/bridge/contracts/test/TestDaiMintSpell.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2021 Dai Foundation
+pragma solidity ^0.6.11;
+
+interface MintLike {
+  function mint(address to, uint256 value) external;
+}
+
+/**
+ * An example spell to mint some dai.
+ */
+contract TestDaiMintSpell {
+  function mintDai(
+    address _dai,
+    address _user,
+    uint256 _amount
+  ) external {
+    MintLike(_dai).mint(_user, _amount);
+  }
+}
