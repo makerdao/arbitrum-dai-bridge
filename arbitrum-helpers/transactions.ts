3ba91b7c69f9921fa473e2b0d2e4812e7f4c8ebf
Krzysztof Kaczor <krzkaczor@gmail.com>
2021-09-16 20:22:44 +0200

    Remove tmp fix for arbitrum network instablity

--- arbitrum-helpers/transactions.ts
+++ arbitrum-helpers/transactions.ts
@@ -1,7 +1,5 @@
 import { ContractTransaction, providers } from 'ethers'
 
-import { delay } from './networks/RetryProvider'
-
 export async function waitForTx(
   tx: Promise<ContractTransaction>,
   _confirmations?: number,
@@ -9,12 +7,6 @@ export async function waitForTx(
   const resolvedTx = await tx
   const confirmations = _confirmations ?? chainIdToConfirmationsNeededForFinalization(resolvedTx.chainId)
 
-  // there's a tmp bug on arbitrum testnet
-  if (resolvedTx.chainId === 421611) {
-    console.log('Waiting for arbitrum tx to finalize...')
-    await delay(60000)
-    console.log('Waiting for arbitrum tx to finalize... DONE')
-  }
   // we retry .wait b/c sometimes it fails for the first time
   try {
     return await resolvedTx.wait(confirmations)
