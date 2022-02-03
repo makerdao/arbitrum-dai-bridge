c339287a661643f5fbeaec0bae05995d4c72a577
olivdb <olivdb@protonmail.com>
2022-02-03 14:05:58 +0000

    Revert "Remove operator fee"
    This reverts commit 0077f669efe1a306f629ea6a3e5324ceaefb211e.

--- test/l1/L1DaiWormholeGateway.ts
+++ test/l1/L1DaiWormholeGateway.ts
@@ -127,6 +127,7 @@ describe('L1DaiWormholeGateway', () => {
         JSON.stringify(Object.values(wormhole).map((v: any) => v.toString())),
       )
       expect(routerSettleCallData.maxFeePercentage).to.equal(0)
+      expect(routerSettleCallData.operatorFee).to.equal(0)
     })
 
     it('reverts when not called by the bridge', async () => {
